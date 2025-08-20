1758ea73810658b669c41cef1cf79d8275c636c6
Nahid Islam <ni749954@gmail.com>
2025-08-20 22:58:36 +0600

    Update SensorPage.js

--- src/SensorPage.js
+++ src/SensorPage.js
@@ -7,7 +7,7 @@ function SensorPage() {
       <h1>📡 সেন্সর ডাটা</h1>
       <iframe
         title="Sensor Data"
-        src="http://192.168.251.84/"  // replace with your sensor web server
+        src="http://192.168.251.84"  // replace with your sensor web server
         width="100%"
         height="600px"
         style={{ border: "2px solid #ccc", borderRadius: "8px" }}
